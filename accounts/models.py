05335f4821ef235f7e391ff14561106736b60518
shakilahmmedms <shakilahmmed8906@gmail.com>
2024-08-07 20:46:44 +0600

    some error solve

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,12 @@ from book_post.models import BookPosts, BuyNow
 
     
 class UserAccounts(models.Model):
-    user = models.OneToOneField(User, related_name = 'account', on_delete = models.CASCADE)
+    user = models.OneToOneField(User, related_name ='account', on_delete = models.CASCADE)
     account_no = models.IntegerField(unique = True)
     initial_deposit_date = models.DateField(auto_now_add = True)
     balance = models.DecimalField(default = 0,max_digits =12,decimal_places = 2)
     buy_history = models.ManyToManyField(BuyNow)
+
     def __str__(self):
         return str(self.account_no)
     
